1d6c8a91e5c2ac6ed2816a0bcfc1bad2ed52a872
Jean <jepaalgo00@gmail.com>
2024-06-07 14:23:10 -0400

    erro imagem

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/style/*.scss')
-        .pipe(sass( {outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/style'))     
+    return gulp.src('./src/style/main.scss') // Compile apenas o main.scss
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/style'));
 }
 
-function images(){
+function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.defaut = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function () {
-    gulp.watch('./src/style/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/style/**/*.scss', gulp.parallel(styles)); // Observe todos os arquivos SCSS
 }
 
-gulp.task('default', gulp.series(styles));
+gulp.task('default', gulp.series(styles, images));
