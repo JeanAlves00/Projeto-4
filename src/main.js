40b11fe1c8c6c119b6cfe3ed7a2c80cd0224380a
Jean <jepaalgo00@gmail.com>
2024-06-10 14:14:34 -0400

    .

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {  
+document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao) {
-            const abaAlvo = botao.target.dataset.tabButton;
+        buttons[i].addEventListener('click', function() {
+            const abaAlvo = this.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
-            escondeTodosAsAbas();
-            aba.classList.add('shows__tabs__button--is-active');
-        })
+            escondeTodasAsAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            this.classList.add('shows__tabs__button--is-active');
+        });
     }
-})
+});
 
+function removeBotaoAtivo () {
+    const buttons = document.querySelectorAll('[data-tab-button]');
 
-function escondeTodosAsAbas() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAsAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
