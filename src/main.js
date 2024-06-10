fde0368827a9743ea0cf756401d586d1cc374c97
Jean <jepaalgo00@gmail.com>
2024-06-10 09:59:41 -0400

    erro

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-document addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('[data-bs-button=em_breve]');
+document.addEventListener('DOMContentLoaded', function() {  
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+            escondeTodosAsAbas();
+            aba.classList.add('shows__tabs__button--is-active');
+        })
+    }
 })
+
+
+function escondeTodosAsAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
